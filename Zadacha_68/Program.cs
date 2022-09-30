75f4189fefd11bfc53b9d179e14d8f764c193aca
Roman Magafurov <roman.mag1997@yandex.ru>
2022-10-01 00:44:11 +0500

    Updated Zadacha_68 Complete

--- Zadacha_68/Program.cs
+++ Zadacha_68/Program.cs
@@ -15,36 +15,6 @@ int n = Convert.ToInt32(Console.ReadLine());
 
 long AckermannFunction(long m, long n)
 {
-    // if (m == 0) n++;
-    // if (m > 0 && n == 0) AckermannFunction(m - 1, n);
-    // if (m > 0 && n > 0) AckermannFunction(m - 1, AckermannFunction(m, n - 1));
-    // return m;
-
-    // if (m < 0 || n < 0) throw new ArgumentOutOfRangeException();
-    // if (m == 0) return n = n + 1;
-    // if (n == 0) return AckermannFunction(m - 1, n);
-    // return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
-
-    // if (n == 0) return m + 1;
-    // else if (n != 0 && m == 0) return AckermannFunction(n - 1, 1);
-    // else return AckermannFunction(n - 1, AckermannFunction(n, m - 1));
-
-
-
-    
-    // restart:
-    //     if (m == 0)
-    //         return  n+1;
-    //     if (n == 0)
-    //     {
-    //         m--;
-    //         n = 1;
-    //         goto restart;
-    //     }
-    //     else
-    //         return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
-
-
     if (m > 0)
     {
         if (n > 0)
@@ -59,13 +29,8 @@ long AckermannFunction(long m, long n)
     }
 
     throw new System.ArgumentOutOfRangeException();
-
-
-
-
 }
 
-
-
-
 Console.WriteLine(AckermannFunction(m, n));
+
+throw new System.ArgumentOutOfRangeException();
