0dc91451c8b074e7ca06e38407c2c1305f919ba2
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-30 20:44:06 +0500

    Updated Complete Zadacha_64

--- Zadacha_64/Program.cs
+++ Zadacha_64/Program.cs
@@ -6,3 +6,23 @@
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 using MyClassLibrary;
+
+Console.Write("Введите целые числа N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+int RecMethod(int N)
+{   
+    Console.Write($"{N}");
+    if(N > 2) {
+        Console.Write(", ");
+        RecMethod(N-1);
+    }
+    else Console.Write(", ");
+    if (N <= 1) return 0;
+    return 1;
+}
+
+Console.Write('"');
+Console.Write(RecMethod(N));
+Console.WriteLine('"');
