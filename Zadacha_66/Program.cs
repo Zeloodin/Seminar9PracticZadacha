f780a5760ebc33eb5622e38c296b2b2a6840ab75
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-30 20:05:55 +0500

    Updated Complete Zadacha_66

--- Zadacha_66/Program.cs
+++ Zadacha_66/Program.cs
@@ -6,3 +6,23 @@
 //
 // M = 4; N = 8. -> 30
 
+using MyClassLibrary;
+
+Console.Write("Введите целое число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+int NaturalSummaRangeMethodRecursion(int M, int N, int value = 0)
+{
+    if(M <= N)
+    {
+        return NaturalSummaRangeMethodRecursion(M+1,N,value+M);
+    }
+    return value;
+}
+
+int value = NaturalSummaRangeMethodRecursion(M, N);
+Console.WriteLine($"Сумма натуральных чисел в диапазоне между {M} и {N}, составляет: {value}");
+
